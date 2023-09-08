ad57bf8c03c897d709f4b742d103ff38d1e34ce7
Ishtiaq AMjad <muhammadishtiaqamjad@gmail.com>
2023-09-08 10:51:03 +0500

    final.101

--- src/App.js
+++ src/App.js
@@ -2,25 +2,14 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import PdfTextExtractor from './components/PdfTextExtractor';
-import { makeStyles } from '@mui/styles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ImageTextExtractor from './components/ImageTextExtractor';
 
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    background: 'linear-gradient(90deg, rgba(2,142,253,0.9892157546612395) 20%, rgba(0,191,179,1) 85%)',
-    height: '100%',
-
-  },
-});
 
 function App() {
   const classes = useStyles();
   return (
-    <div className={`${classes.root} "App"`}>
+    <div className='Approot App'>
       <BrowserRouter>
         <Header />
         <Routes>
